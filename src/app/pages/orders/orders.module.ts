7fe75026eb193fb965ece953c4eb87dafff9aefd
Ahmed Nazeh <a.nazih@dttmoj.onmicrosoft.com>
2019-11-04 15:16:18 +0200

    add camera plugin

--- src/app/pages/orders/orders.module.ts
+++ src/app/pages/orders/orders.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { OrdersPage } from './orders.page';
+import { Camera } from '@ionic-native/camera/ngx';
 
 const routes: Routes = [
   {
@@ -19,7 +20,11 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    
     RouterModule.forChild(routes)
+  ],
+  providers:[
+    Camera
   ],
   declarations: [OrdersPage]
 })
--- src/app/pages/orders/orders.page.ts
+++ src/app/pages/orders/orders.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersPage implements OnInit {
 
-  constructor() { }
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
   }
+ openCamera(){
+  const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  
+  this.camera.getPicture(options).then((imageData) => {
+   // imageData is either a base64 encoded string or a file URI
+   // If it's base64 (DATA_URL):
+   let base64Image = 'data:image/jpeg;base64,' + imageData;
+   console.log(base64Image);
+  }, (err) => {
+   // Handle error
+  });
+ }
 
+ AccessCamera(){
+
+  this.camera.getPicture({
+ 
+  targetWidth:512,
+ 
+  targetHeight:512,
+ 
+  correctOrientation:true,
+ 
+  sourceType: this.camera.PictureSourceType.CAMERA,
+ 
+  destinationType: this.camera.DestinationType.DATA_URL
+ 
+    }).then((imageData) => {
+ 
+    console.log(imageData);
+ 
+          }, (err) => {
+ 
+      console.log(err);
+ 
+    });
+ 
+ }
+ 
+ AccessGallery(){
+ 
+  this.camera.getPicture({
+ 
+     sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+ 
+     destinationType: this.camera.DestinationType.DATA_URL
+ 
+    }).then((imageData) => {
+ 
+      console.log(imageData);
+
+ 
+         }, (err) => {
+ 
+      console.log(err);
+ 
+    });
+ 
+ }
+ 
+ 
 }
