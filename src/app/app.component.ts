c73dac4c14b7122bb6ab022c9cf71ba6f90b1872
Ahmed Nazeh <a.nazih@dttmoj.onmicrosoft.com>
2019-11-10 01:34:12 +0200

    change menu design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   public appPages = [
-    {title: 'splash',url: '/splash',icon: 'home'},
+   
     {title: 'Home',url: '/home',icon: 'home'},
-    {title: 'auth',url: '/auth',icon: 'home'},
-    {title: 'city',url: '/city',icon: 'home'},
-    {title: 'thanks',url: '/thanks',icon: 'home'},
-    {title: 'basket off',url: '/basket-off',icon: 'home'},
-    {title: 'orders',url: '/orders',icon: 'home'},
-    {title: 'place order',url: '/place-order',icon: 'home'},
-    {title: 'account information',url: '/account-info',icon: 'home'},
-    {title: 'basket history',url: '/basket-history',icon: 'home'},
+    {title: 'account information',url: '/account-info',icon: 'contact'},
+    {title: 'basket history',url: '/basket-history',icon: 'basket'},
   ];
 
   constructor(
